ecbfd9948b2b75d5e6629e2c0099254d2d1c79e3
Heng Xin Phei <147351430+xinphei@users.noreply.github.com>
2024-05-05 23:26:43 +0800

    Add files via upload

---
+++ Labs/23005228/Week 5/Tutorial/Node3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tutorial;
+
+/**
+ *
+ * @author xinph
+ */
+public class Node3 {
+    char data;
+    Node next;
+    Node prev;
+    
+    public Node3(char data, Node next, Node prev) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+        
+    }
+}
